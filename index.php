ad177ce3b19353383dcc49c89f8626672526156b
Tauheed <tauheedsheikh120@gmail.com>
2022-09-06 12:59:23 -0700

    "Index.php update but backgroung img is not Work Please Check This Out"

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php 
 include('./assets/template/user/header.php')
 ?>    
-
+<!-- Css Link -->
+<link rel="stylesheet" href="./assets/CSS/userCss/home.css">
 <!-- Main Content Starts Here -->
-
+<div class="hero">
+    <div class="highway"></div>
+    <div class="city"></div>
+    <div class="car">
+        <img src="./assets/img/userimg/car.png" alt="" srcset="">
+    </div>
+    <div class="wheel">
+        <img class="backwheel" src="./assets/img/userimg/wheel.png" alt="">
+        <img class="frontwheel" src="./assets/img/userimg/wheel.png" alt="">
+    </div>
+</div>
 <!-- Main Content Ends Here -->
 
 <?php 
