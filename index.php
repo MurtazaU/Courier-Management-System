b360479839bf101bed7f0cc29222afb39e8a1a7e
Drago78624 <maazahmed78624@gmail.com>
2022-09-06 18:45:37 +0500

    Template-Modified

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- Bootstrap Link -->
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+include('./assets/template/user/header.php')
+?>    
+
+<!-- Main Content Starts Here -->
+
+<!-- Main Content Ends Here -->
+
+<?php 
+include('./assets/template/user/footer.php')
+?>    
