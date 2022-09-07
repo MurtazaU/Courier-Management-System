9aabadf321fb514f89b0b41c0468d4628fead148
Murtaza Usmani <murtazausmani985@gmail.com>
2022-09-07 14:23:07 +0500

    Hero-Created

--- index.php
+++ index.php
@@ -1,20 +1,43 @@
 <?php 
 include('./assets/template/user/header.php')
-?>    
+?>
+<!-- Links to External Resources Starts Here -->
 <!-- Css Link -->
 <link rel="stylesheet" href="./assets/CSS/userCss/home.css">
-<!-- Main Content Starts Here -->
-<div class="hero">
-    <div class="highway"></div>
-    <div class="city"></div>
-    <div class="car">
-        <img src="./assets/img/userimg/car.png" alt="" srcset="">
-    </div>
-    <div class="wheel">
-        <img class="backwheel" src="./assets/img/userimg/wheel.png" alt="">
-        <img class="frontwheel" src="./assets/img/userimg/wheel.png" alt="">
-    </div>
-</div>
+<!-- Links to External Resources Ends Here -->
+
+
+<!-- Hero Section Starts Here -->
+    <main>
+      <section class="section-hero section">
+        <div class="container ">
+            <div class="row">
+                <div class="col-6">
+                    <div class="section-hero-data">
+                        <p class="hero-top-data">this is me</p>
+                        <h1 class="hero-heading">thapa technical</h1>
+                        <p class="hero-para">
+                        I'm vinod bahadur thapa. A Full stack Developer, youtuber and
+                        freelancer. A Full stack Developer, youtuber and freelancer.
+                        </p>
+                    </div>
+                </div>
+                    
+                    <!-- hero section right side  -->
+                    <div class="col-6">
+                    <div class="section-hero-image">
+                        <!-- Hero Img -->
+                            <img src="./assets/img/userimg/hero-img.png" alt="Package Being Delivered">
+                            <!-- Hero Img Ends -->
+                    </div>
+                    </div>
+            </div>
+         
+        </div>
+      </section>
+    </main>
+<!-- Hero Section Ends Here -->
+
 <!-- Main Content Ends Here -->
 
 <?php 
