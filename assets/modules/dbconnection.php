b6b68af4271c99b9d85495df608cdeaa0c8e7edd
Murtaza Usmani <murtazausmani985@gmail.com>
2022-09-15 17:00:45 +0500

    Db-Connection-initialized

---
+++ assets/modules/dbconnection.php
@@ -0,0 +1,16 @@
+<?php
+
+// 4 Variables
+$dbname = 'courierms';
+$host = "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $con = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+?>
